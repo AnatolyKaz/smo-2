55f194d7a42dfc3a08b4b65ce4909a40e3a69178
AnatolyKaz <ankaz.work@gmail.com>
2021-04-12 16:03:54 +0500

    major fix

--- smo.js
+++ smo.js
@@ -1,56 +1,73 @@
-const time = 1000000
+const time = 1000000 //мс
 const lambda = 0.0000094
 const mu = 0.000014
-const eta = 0.000014
+//const eta = 0.000014
 const n = 3
-const channels = { channels: n, freeChannels: n }
+const channels = {channels: n, freeChannels: n}
 const l = 3
 
-let countApps = 0
-let timeNewApp = 0
-let refusalAppsCount = 0
-let freeTimeCount = 0
-let orderInterval = 0
-let apps = []
+let countApps = [] //обслуженные заявки
+let timeNewApp = 0 //время прихода новой заявки
+let refusalAppsCount = 0 // количество заявок с отказом
+let freeTimeCount = 0 //время простоя системы
+let apps = [] //массив заявок на обслуживании
 
+for (let i = 0; i <= time; i++) {
+    if (!apps.length) {
+        freeTimeCount++
+    }
 
-for (let i = 0 ; i <= time ; i++) {
+    timeNewApp = i ? timeNewApp : getTimeNewApp()
 
-	if ( i <= timeNewApp + orderInterval ) {
-		calcTimeNewApp(i)
-	}
+    if (i === timeNewApp) {
+        timeNewApp = getTimeNewApp()
 
+        if (!i) {
 
-	if ( !apps.length ) {
-		freeTimeCount++
-	}
+            if (apps.length === n) {
+                refusalAppsCount++
+            } else {
+                if (!apps.length && l <= n){
+                    apps.push({
+                        serviceTime: getServiceTime(l),
+                        arrivalTimeApp: i
+                    })
+                }
 
-	if ( i === timeNewApp ) {
-		if ( apps.length < channels.channels ) {
-			let newApp = { serviceTime: Math.floor(-( 1 / ( l * mu + eta ) ) * Math.log(Math.random())) }
-			apps.push(newApp)
-		} else {
-			refusalAppsCount++
-		}
-	}
+                if (n >= (2*l) && apps.length === 1){
+                    apps.push({
+                        serviceTime: getServiceTime(l),
+                        arrivalTimeApp: i
+                    })
+                }
 
-	if ( apps.length ) {
-		apps.forEach((app) => {
-			if ( channels.freeChannels === channels.channels ) {
-				app.serviceTime = app.serviceTime - ( channels.freeChannels - 1 )
-			} else {
+                if ((apps.length + 1)*l <= n){
+                    apps.push({
+                        serviceTime: getServiceTime(l),
+                        arrivalTimeApp: i
+                    })
+                }
 
-			}
-		})
-	}
+                if((apps.length + 1)*l > n && apps.length < n){
+
+                    apps = apps.map( app => {
+                        app.serviceTime = i - app.arrivalTimeApp + getServiceTime()
+                    })
+                }
+
+            }
+        }
+
+    }
 
 
 }
 
-function calcTimeNewApp(i) {
-	let newTime = i + Math.floor(-( 1 / l * lambda ) * Math.log(Math.random()))
-	timeNewApp = newTime < time ? newTime : timeNewApp
-	orderInterval = Math.floor(-( 1 / lambda ) * Math.log(Math.random()))///25000 мс
+
+function getTimeNewApp() {
+    return (-1 / lambda) * Math.log(Math.random())
 }
 
-console.log(Math.floor(-( 1 / ( l * lambda ) ) * Math.log(Math.random())))
+function getServiceTime(l) {
+    return (-1 / (l * mu)) * Math.log(Math.random())
+}
